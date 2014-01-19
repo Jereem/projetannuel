275801cfe00215284156f19e37efbc3e01b23a53
Teddy Delavallee <teddy.delavallee@gmail.com>
2014-01-19 21:34:32 +0100

    ajout adherents.xhtml fonctionnel non finit

--- src/java/beans/Etudiant.java
+++ src/java/beans/Etudiant.java
@@ -3,9 +3,15 @@ package beans;
 import java.util.ArrayList;
 import java.util.Date;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import tools.ConnectBDD;
 
 @ManagedBean
 @RequestScoped
@@ -107,5 +113,54 @@ public class Etudiant extends Personne implements Serializable{
 	public void setAJourCotisation(boolean AJourCotisation) {
 		throw new UnsupportedOperationException();
 	}
+        
+            // Methodes pour la BDD
+    public String saveEtudiant() throws SQLException {
+        ConnectBDD b = new ConnectBDD();
+        if (b == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        try {
+            /* Récupération des paramètres d'URL saisis par l'utilisateur */
+            String paramTitre = this.getTitre().toString();
+            String paramNom = this.getNom();
+            String paramPrenom = this.getPrenom();
+            //String paramCoordonnees = this.getCoordonnees().toString();
+            int paramNumeroSS = this.getNumeroSS();
+            //Date paramdateNaissance = this.getDateNaissance();
+            /* Exécution d'une requête de modification de la BD (INSERT, UPDATE, DELETE, CREATE, etc.). */
+            b.getMyStatement().executeUpdate("INSERT INTO projetannuel.personne(Titre, Nom_Personne, Prenom_Personne, Numero_SS) VALUES (" + paramTitre + "," + paramNom + "," + paramPrenom + "," + paramNumeroSS + ")");
+            
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            
+        }
+        return "success";
+    }
+
+      public List<BureauProGphy> getBureauProGphy() throws SQLException {
+        //get database connection
+        ConnectBDD b = new ConnectBDD();
+        Connection con = b.getMyConnexion();
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        PreparedStatement ps = con.prepareStatement("select Identifiant, Mot_de_passe, Actif from membre_bureau");
+        //get customer data from database
+        ResultSet result = ps.executeQuery();
+        List<BureauProGphy> list = new ArrayList<BureauProGphy>();
+        while (result.next()) {
+            BureauProGphy membre = new BureauProGphy();
+            membre.setIdentifiant(result.getString("Identifiant"));
+            membre.setMdp(result.getString("Mot_de_passe"));
+            //membre.setPoste(result.getString("Poste"));
+            membre.setActif(result.getBoolean("Actif"));
+            //store all data into a List
+            list.add(membre);
+        }
+        return list;
+    }
 
 }
