6ac27c1eecf2b8d621ade8400a344234370d9be0
Teddy Delavallee <teddy.delavallee@gmail.com>
2014-01-20 21:33:44 +0100

    Creation en cours de la page adherents (formulaire coordonnées finit)

--- src/java/beans/Coordonnees.java
+++ src/java/beans/Coordonnees.java
@@ -1,4 +1,5 @@
 package beans;
+
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -7,128 +8,141 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @RequestScoped
 
-public class Coordonnees implements Serializable{
+public class Coordonnees implements Serializable {
 
-	private String rue;
-	private Voies typeVoie;
-	private int cp;
-	private String ville;
-	private String pays;
-	private int numRue;
-	private String eMail;
-	private int telPortable;
-	private int telFixe;
+    protected enum Voies implements Serializable {
 
-	public Coordonnees() {
-		throw new UnsupportedOperationException();
-	}
+        Rue, Boulevard, LieuDit, Avenue, Allee, Impasse, Quai;
 
-	public String getRue() {
-		return this.rue;
-	}
+        public String getNomVoie() {
+            return toString();
+        }
+    }
 
-	/**
-	 * 
-	 * @param rue
-	 */
-	public void setRue(String rue) {
-		this.rue = rue;
-	}
+    protected final Voies[] voies = Voies.values();
 
-	public Voies getTypeVoie() {
-		return this.typeVoie;
-	}
+    private String rue;
+    private Voies typeVoie;
+    private int codePostal;
+    private String ville;
+    private String pays;
+    private int numRue;
+    private String eMail;
+    private int telPortable;
+    private int telFixe;
 
-	/**
-	 * 
-	 * @param typeVoie
-	 */
-	public void setTypeVoie(Voies typeVoie) {
-		this.typeVoie = typeVoie;
-	}
+    public Voies[] getVoies() {
+        return voies;
+    }
 
-	public int getCP() {
-		throw new UnsupportedOperationException();
-	}
+    
 
-	/**
-	 * 
-	 * @param cp
-	 */
-	public void setCP(int cp) {
-		throw new UnsupportedOperationException();
-	}
+    public String getRue() {
+        return this.rue;
+    }
 
-	public String getVille() {
-		return this.ville;
-	}
+    /**
+     *
+     * @param rue
+     */
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
 
-	/**
-	 * 
-	 * @param ville
-	 */
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
+    public Voies getTypeVoie() {
+        return this.typeVoie;
+    }
 
-	public String getPays() {
-		return this.pays;
-	}
+    /**
+     *
+     * @param typeVoie
+     */
+    public void setTypeVoie(Voies typeVoie) {
+        this.typeVoie = typeVoie;
+    }
 
-	/**
-	 * 
-	 * @param pays
-	 */
-	public void setPays(String pays) {
-		this.pays = pays;
-	}
+    public int getCodePostal() {
+        return this.codePostal;
+    }
 
-	public int getNumRue() {
-		return this.numRue;
-	}
+    /**
+     *
+     * @param cp
+     */
+    public void setCodePostal(int cp) {
+        this.codePostal = cp;
+    }
 
-	/**
-	 * 
-	 * @param numRue
-	 */
-	public void setNumRue(int numRue) {
-		this.numRue = numRue;
-	}
+    public String getVille() {
+        return this.ville;
+    }
 
-	public String getEMail() {
-		return this.eMail;
-	}
+    /**
+     *
+     * @param ville
+     */
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
 
-	/**
-	 * 
-	 * @param eMail
-	 */
-	public void setEMail(String eMail) {
-		this.eMail = eMail;
-	}
+    public String getPays() {
+        return this.pays;
+    }
 
-	public int getTelPortable() {
-		return this.telPortable;
-	}
+    /**
+     *
+     * @param pays
+     */
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
 
-	/**
-	 * 
-	 * @param telPortable
-	 */
-	public void setTelPortable(int telPortable) {
-		this.telPortable = telPortable;
-	}
+    public int getNumRue() {
+        return this.numRue;
+    }
 
-	public int getTelFixe() {
-		return this.telFixe;
-	}
+    /**
+     *
+     * @param numRue
+     */
+    public void setNumRue(int numRue) {
+        this.numRue = numRue;
+    }
 
-	/**
-	 * 
-	 * @param telFixe
-	 */
-	public void setTelFixe(int telFixe) {
-		this.telFixe = telFixe;
-	}
+    public String getEMail() {
+        return this.eMail;
+    }
+
+    /**
+     *
+     * @param eMail
+     */
+    public void setEMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public int getTelPortable() {
+        return this.telPortable;
+    }
+
+    /**
+     *
+     * @param telPortable
+     */
+    public void setTelPortable(int telPortable) {
+        this.telPortable = telPortable;
+    }
+
+    public int getTelFixe() {
+        return this.telFixe;
+    }
+
+    /**
+     *
+     * @param telFixe
+     */
+    public void setTelFixe(int telFixe) {
+        this.telFixe = telFixe;
+    }
 
 }
