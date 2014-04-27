5327a15f576418acf1b397e3bdfb39c305b883a3
Audrey <Audrey@Audrey.home>
2014-04-27 20:32:32 +0200

    ajout fonction getClient dans la classe Client.java et modification dans clients.xhtml pour l'affichage

--- src/java/beans/Client.java
+++ src/java/beans/Client.java
@@ -54,4 +54,39 @@ public class Client extends Personne implements Serializable{
 		throw new UnsupportedOperationException();
 	}
 
+        public List<Client> getClient() throws SQLException {
+        //get database connection
+        ConnectBDD b = new ConnectBDD();
+        Connection con = b.getMyConnexion();
+        if (con == null) {
+            throw new SQLException("Can't get database connection");
+        }
+        PreparedStatement ps = con.prepareStatement("select * from projetannuel.CLIENT natural join projetannuel.PERSONNE natural join projetannuel.COORDONNEES");
+        //get customer data from database
+        ResultSet result = ps.executeQuery();
+        List<Client> list = new ArrayList<>();
+        while (result.next()) {
+            Client client = new Client();
+            client.setTitres(result.getString("Titre"));
+            client.setNom(result.getString("Nom_Personne"));
+            client.setPrenom(result.getString("Prenom_Personne"));
+            Coordonnees coordonnes = new Coordonnees();
+            coordonnes.setNumRue(result.getInt("Numero_De_Rue"));
+            coordonnes.setVoies(result.getString("Type_Voie"));
+            coordonnes.setRue(result.getString("Rue"));
+            coordonnes.setCodePostal(result.getString("Code_Postal"));
+            coordonnes.setVille(result.getString("Ville"));
+            coordonnes.setPays(result.getString("Pays"));
+            coordonnes.seteMail(result.getString("Email"));
+            coordonnes.setTelFixe(result.getInt("Telephone_1"));
+            coordonnes.setTelPortable(result.getInt("Telephone_2"));
+            client.setCoordonnees(coordonnes);
+//            System.out.println(adherent.getNom());
+//            System.out.println("test");
+            //store all data into a List
+            list.add(client);
+        }
+//        System.out.println(list);
+        return list;
+    }
 }
